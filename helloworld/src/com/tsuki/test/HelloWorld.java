9f904713316fc26b74c94085ee5fab9c5eedf1b7
Tsuki <tsukiikust@163.com>
2026-05-24 13:39:59 +0800

    Initial commit

---
+++ helloworld/src/com/tsuki/test/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.tsuki.test;
+//类定义在哪个包下
+
+//定义一个类class
+public class HelloWorld {
+
+    //java的主入口。。
+    static void main(String[] args) {
+        System.out.println("hello world");
+    }
+
+
+
+}

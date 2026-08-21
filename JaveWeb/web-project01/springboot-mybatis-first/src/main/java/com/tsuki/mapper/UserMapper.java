8f2a9b599ea7be45858454a7d9564f27629e0ba1
Tsuki <tsukiikust@163.com>
2026-08-21 22:40:42 +0800

    mybatis

--- JaveWeb/web-project01/springboot-mybatis-first/src/main/java/com/tsuki/mapper/UserMapper.java
+++ JaveWeb/web-project01/springboot-mybatis-first/src/main/java/com/tsuki/mapper/UserMapper.java
@@ -1,8 +1,7 @@
 package com.tsuki.mapper;
 
 import com.tsuki.pojo.User;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.*;
 
 import java.util.List;
 
@@ -15,4 +14,17 @@ public interface UserMapper {
     //返回值: 查询结果
     @Select("select * from user")
     public List<User> findAll();
+
+    @Delete("delete from user where id = #{id}")
+    public void deleteById(Integer id);
+
+    @Insert("insert into user (username, password, name, age) values (#{username}, #{password}, #{name}, #{age})")
+    public void insert(User user);
+
+    @Update("update user set username = #{username}, password = #{password}, name = #{name}, age = #{age} where id = #{id}")
+    public void update(User user);
+
+    @Select("select * from user where username = #{username} and password = #{password}")
+    public User findByUsernameAndPassword(@Param("username") String username, @Param("password") String password);
+
 }
--- JaveWeb/web-project01/springboot-mybatis-first/src/test/java/com/tsuki/springbootmybatisfirst/SpringbootMybatisFirstApplicationTests.java
+++ JaveWeb/web-project01/springboot-mybatis-first/src/test/java/com/tsuki/springbootmybatisfirst/SpringbootMybatisFirstApplicationTests.java
@@ -21,6 +21,29 @@ class SpringbootMybatisFirstApplicationTests {
         users.forEach(System.out::println);
     }
 
+    @Test
+    void testDeleteById() {
+        userMapper.deleteById(5);
+    }
+
+    @Test
+    void testInsert() {
+        User user = new User(null,"admin", "123456", "张三", 18);
+        userMapper.insert(user);
+    }
+
+    @Test
+    void testUpdate() {
+        User user = new User(1, "niko", "123456", "张三", 18);
+        userMapper.update(user);
+    }
+
+    @Test
+    void testFindByUsernameAndPassword() {
+        User user = userMapper.findByUsernameAndPassword("niko", "123456");
+        System.out.println(user);
+    }
+
 
 
 }
